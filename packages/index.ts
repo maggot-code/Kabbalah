3369333bd903dfe7a83959957727d0b768f54468
maggot_code <wowhuanhuan233@163.com>
2021-04-21 11:21:51 +0800

    #

--- packages/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: maggot-code
- * @Date: 2021-04-21 09:56:12
- * @LastEditors: maggot-code
- * @LastEditTime: 2021-04-21 09:56:23
- * @Description: file content
- */
-module.exports = {}
