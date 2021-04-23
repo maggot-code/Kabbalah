eb6c1e110619b50ba732bae28cfbfc110b32b4c1
maggot_code <wowhuanhuan233@163.com>
2021-04-23 15:44:42 +0800

    #

--- packages/k-count/domain/count.vue
+++ packages/k-count/domain/count.vue
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2021-04-22 09:43:08
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-04-23 15:15:13
+ * @LastEditTime: 2021-04-23 15:38:29
  * @Description: file content
 -->
 <template>
@@ -16,10 +16,3 @@ export default defineComponent({
     setup() {},
 })
 </script>
-<style lang='scss' scoped>
-.k-cout {
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-}
-</style>
