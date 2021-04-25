5b4dafd6cc0a0119082b9646a6d684b3456cab9c
maggot_code <wowhuanhuan233@163.com>
2021-04-25 11:30:44 +0800

    # feat：add vue3.0 + vite debug test tool

---
+++ debug/src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: maggot-code
+ * @Date: 2021-04-25 09:47:21
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-04-25 11:26:14
+ * @Description: file content
+-->
+<template>
+    <k-popup></k-popup>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { KPopup } from '../../packages/install'
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        KPopup,
+    },
+})
+</script>
