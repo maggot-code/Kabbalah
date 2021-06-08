2a6103d5a2a24ff5e69bcba4928b6e3519039161
maggot_code <wowhuanhuan233@163.com>
2021-06-08 14:29:42 +0800

    #

--- packages/directive/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: maggot-code
- * @Date: 2021-04-26 13:59:50
- * @LastEditors: maggot-code
- * @LastEditTime: 2021-04-26 14:02:01
- * @Description: file content
- */
-export { default as Pop } from './pop'
--- packages/utils/isServer.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: maggot-code
- * @Date: 2021-04-25 13:20:33
- * @LastEditors: maggot-code
- * @LastEditTime: 2021-04-25 13:20:53
- * @Description: file content
- */
-export default typeof window === 'undefined';
